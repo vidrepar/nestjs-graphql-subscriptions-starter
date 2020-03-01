b0dead176eefa7e243e112b343329ad9bd1baa5c
Vid <vid.repar@gmail.com>
2020-03-01 10:16:07 +0100

    feat: Add a working ping pong example

--- src/ticket.resolver.ts
+++ src/ticket.resolver.ts
@@ -1,5 +1,5 @@
 import { Field, ID, ObjectType } from 'type-graphql';
-import { Query, Mutation, Resolver, Subscription } from '@nestjs/graphql';
+import { Query, Mutation, Resolver, Subscription, Args } from '@nestjs/graphql';
 import { PubSub } from 'apollo-server-express';
 
 @ObjectType()
@@ -25,8 +25,28 @@ export class TicketResolver {
     return { id: pingId + 'bar' };
   }
 
-  @Subscription(returns => Ticket)
-  pong() {
+  // payload seems to be what you send on publish pubSub.publish(... usually in mutation
+  // variables seem to be the filter/variables/args you provide when opening a subscription
+  @Subscription(returns => Ticket, {
+    filter(this: TicketResolver, payload, variables, foo) {
+      // "this" refers to an instance of "AuthorResolver"
+      // return payload.commentAdded.repositoryName === variables.repoFullName;
+
+      console.log('payload');
+      console.log(payload);
+
+      console.log('variables');
+      console.log(variables);
+
+      console.log('foo'); // foo is metadata
+      console.log(foo);
+
+      return true;
+    },
+  })
+  pong(
+    @Args({ name: 'repoFullName', type: () => String }) repoFullName: string,
+  ) {
     return pubSub.asyncIterator(PONG_EVENT_NAME);
   }
 }
